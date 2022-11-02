bf5fac0e08ec5dce523054e8ffe55f3726523d3f
Moahmedahmadian <42762811+Moahmedahmadian@users.noreply.github.com>
2022-11-02 13:01:28 +0330

    add dotenv module

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { typeDefs } from "./schema";
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+require('dotenv').config()
+
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({
@@ -22,12 +24,15 @@ const server = new ApolloServer({
 //  1. creates an Express app
 //  2. installs your ApolloServer instance as middleware
 //  3. prepares your app to handle incoming requests
+
+
+
 async function start() {
   const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
     context: async ({ req, res }) => ({
       accessToken: req.headers.authorization,
-      jwsSecretKey: "key1",
+      jwsSecretKey: process.env.jwtSecretKey,
     }),
   });
 
